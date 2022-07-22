1c70719d254cc441c03ed15f1936ba020ee43423
Ingo <marmeladekegel@gmail.com>
2022-07-22 13:59:28 +0200

    Hi

--- src/Solution.java
+++ src/Solution.java
@@ -1,126 +1,122 @@
 import java.util.*;
 
 public class Solution {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.isHappy(19));
+        System.out.println(s.titleToNumber("ZYR"));
     }
 
-    private int roman(String s){
+    private int roman(String s) {
 
-            char[]c = s.toCharArray();
-            int erg = 0;
+        char[] c = s.toCharArray();
+        int erg = 0;
 
-            for(int i= 0; i< c.length; i++){
-                switch(c[i]){
-                    case 'I': {
-                        if(i != c.length-1) {
-                            switch (c[i + 1]) {
-                                case 'V':
-                                    erg += 4;
-                                    i++;
-                                    break;
-                                case 'X':
-                                    erg += 9;
-                                    i++;
-                                    break;
-                                default:
-                                    erg++;
-                                    break;
+        for (int i = 0; i < c.length; i++) {
+            switch (c[i]) {
+                case 'I': {
+                    if (i != c.length - 1) {
+                        switch (c[i + 1]) {
+                            case 'V':
+                                erg += 4;
+                                i++;
+                                break;
+                            case 'X':
+                                erg += 9;
+                                i++;
+                                break;
+                            default:
+                                erg++;
+                                break;
 
-                            }
                         }
-                        else erg +=1;
+                    } else erg += 1;
 
-                        break;
-                    }
-                    case 'V': {
-                        erg += 5;
-                        break;
-                    }
-                    case 'X': {
-                        if(i != c.length-1) {
-                            switch (c[i + 1]) {
-                                case 'L':
-                                    erg += 40;
-                                    i++;
-                                    break;
-                                case 'C':
-                                    erg += 90;
-                                    i++;
-                                    break;
-                                default:
-                                    erg += 10;
-                                    break;
+                    break;
+                }
+                case 'V': {
+                    erg += 5;
+                    break;
+                }
+                case 'X': {
+                    if (i != c.length - 1) {
+                        switch (c[i + 1]) {
+                            case 'L':
+                                erg += 40;
+                                i++;
+                                break;
+                            case 'C':
+                                erg += 90;
+                                i++;
+                                break;
+                            default:
+                                erg += 10;
+                                break;
 
-                            }
                         }
-                        else erg +=10;
+                    } else erg += 10;
 
-                        break;
-                    }
-
-                    case 'L': {
-                        erg += 50;
-                        break;
-                    }
-
-                    case 'C': {
-                        if(i != c.length-1) {
-                            switch (c[i + 1]) {
-                                case 'D':
-                                    erg += 400;
-                                    i++;
-                                    break;
-                                case 'M':
-                                    erg += 900;
-                                    i++;
-                                    break;
-                                default:
-                                    erg += 100;
-                                    break;
-
-                            }
-                        }
-                        else erg += 100;
-
-                        break;
-                    }
-                    case 'D': {
-                        erg += 500;
-                        break;
-                    }
-                    case 'M': {
-                        erg += 1000;
-                        break;
-                    }
-
-
-
+                    break;
                 }
 
+                case 'L': {
+                    erg += 50;
+                    break;
+                }
+
+                case 'C': {
+                    if (i != c.length - 1) {
+                        switch (c[i + 1]) {
+                            case 'D':
+                                erg += 400;
+                                i++;
+                                break;
+                            case 'M':
+                                erg += 900;
+                                i++;
+                                break;
+                            default:
+                                erg += 100;
+                                break;
+
+                        }
+                    } else erg += 100;
+
+                    break;
+                }
+                case 'D': {
+                    erg += 500;
+                    break;
+                }
+                case 'M': {
+                    erg += 1000;
+                    break;
+                }
+
+
             }
-            return erg;
+
+        }
+        return erg;
     }
-    private String LongestCommonPrefix( String[] strs){
+
+    private String LongestCommonPrefix(String[] strs) {
         String erg = "";
-        for(int i = 0; i < strs[0].length(); i++){
-            char tmp =  strs[0].charAt(i);
+        for (int i = 0; i < strs[0].length(); i++) {
+            char tmp = strs[0].charAt(i);
             boolean append = true;
-            for(String s : strs){
-                if(i >= s.length()){
+            for (String s : strs) {
+                if (i >= s.length()) {
                     append = false;
                     break;
                 }
-                if(s.charAt(i) != tmp) {
+                if (s.charAt(i) != tmp) {
                     append = false;
                     break;
                 }
             }
-            if(append){
+            if (append) {
                 erg = erg + tmp;
-            }
-            else{
+            } else {
                 break;
             }
 
@@ -129,31 +125,46 @@ public class Solution {
         return erg;
 
     }
-    public boolean isHappy(int n){
+
+    public boolean isHappy(int n) {
         List visited = new ArrayList() {
         };
-        boolean Happy= true;
-        while(Happy){
-          String Zeichen =  Integer.toString(n);
-          int erg = 0;
-          for(char c : Zeichen.toCharArray()){
-              erg += Math.pow(Integer.parseInt(String.valueOf(c)), 2) ;
+        boolean Happy = true;
+        while (Happy) {
+            String Zeichen = Integer.toString(n);
+            int erg = 0;
+            for (char c : Zeichen.toCharArray()) {
+                erg += Math.pow(Integer.parseInt(String.valueOf(c)), 2);
 
-          }
-          if(erg == 1){
-              Happy = true;
-              break;
-          }
-          if(visited.contains(erg)){
-              Happy= false;
-              break;
-          }
-          else{
-              visited.add(erg);
-          }
-          n = erg;
+            }
+            if (erg == 1) {
+                Happy = true;
+                break;
+            }
+            if (visited.contains(erg)) {
+                Happy = false;
+                break;
+            } else {
+                visited.add(erg);
+            }
+            n = erg;
         }
         return Happy;
+    }
+
+    public int titleToNumber(String columnTitle) {
+        char[] c = columnTitle.toCharArray();
+        // A = 10, Z = 35
+        //System.out.println(Character.getNumericValue('Z') -9);
+        int exp = 0;
+        int erg = 0;
+        for (int i = c.length - 1; i >= 0; i--) {
+            erg += (Character.getNumericValue(c[i]) - 9) * (Math.pow(26, exp));
+            exp++;
+
+        }
+        return erg;
+
     }
 }
 
