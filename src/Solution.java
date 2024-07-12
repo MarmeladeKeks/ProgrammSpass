38973bc7d5c02675c4c6de26350dd9cc2c288e51
marmelade <marmeladekegel@gmail.com>
2024-07-12 17:10:34 +0200

    fucking Hell

--- src/Solution.java
+++ src/Solution.java
@@ -20,7 +20,7 @@ public class Solution {
         //System.out.println(s.threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
         //System.out.println(s.longestSubsequence(new int[]{4, 12, 10, 0, -2, 7, -8, 9, -9, -12, -12, 8, 8}, 0));
         //s.merge2(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
-        System.out.println(s.maximumGain("aabbaaxybbaabb", 5, 4));
+        System.out.println(s.maximumGain("cdbcbbaaabab", 4, 5));
 
 //        try {
 //            System.in.read();
@@ -4082,64 +4082,54 @@ public class Solution {
     public int maximumGain(String s, int x, int y) {
         // "ab" = x, "ba" = y
         int result = 0;
+        Stack<Character> characterStack = new Stack<>();
+        String firstString = (x > y) ? "ab" : "ba";
+        String secondString = (x > y) ? "ba" : "ab";
 
-        LinkedList<Character> work = new LinkedList<>();
-        for (int i = 0; i < s.length(); i++){
-            work.add(s.charAt(i));
+
+        // first Pass
+        for (int i = 0; i < s.length(); i++) {
+            result += (checkCanOperate(characterStack, firstString, s.charAt(i))) ? Math.max(x, y) : 0;
         }
-        while(containsTwoStringsInList(work, "ab", "ba")){
-            String firstString = (x > y) ? "ab" : "ba";
-            String secondString = (x > y)? "ba" : "ab";
+        s = stackToString(characterStack);
 
-            if (removeFirstOccurrenceInList(work, firstString))
-                result += Math.max(x, y);
-            else{
-                removeFirstOccurrenceInList(work, secondString);
-                result += Math.min(x, y);
-            }
-
+        //second pass
+        characterStack.clear();
+        for (int i = 0; i < s.length(); i++) {
+            result += (checkCanOperate(characterStack, secondString, s.charAt(i))) ? Math.min(x, y) : 0;
         }
+
         return result;
 
-    }
-    private boolean removeFirstOccurrenceInList(LinkedList<Character> stack, String checkString){
-        int length = checkString.length();
-        for (int i = 0; i < stack.size(); i++) {
-            StringBuilder currentCharsToTest = new StringBuilder();
-            for(int j = 0; j < length; j++){
-                if (j + i >= stack.size())
-                    break;
-                currentCharsToTest.append(stack.get(i + j));
-            }
-            if (currentCharsToTest.length() != length)
-                continue;
-            else{
-                if (currentCharsToTest.toString().equals(checkString)){
-                    for (int j = 0; j < length ; j++) {
-                        stack.remove(i);
-                    }
-                    return true;
-                }
-            }
 
-        }
-        return false;
     }
-    private boolean containsTwoStringsInList(List<Character> myList, String firstString, String secondString){
-        for(int i = 0; i < myList.size(); i++){
-            StringBuilder stringCheck = new StringBuilder();
-            for(int j = 0; j < firstString.length(); j++){
-                if (j + i >= myList.size()){
-                    break;
-                }
-                stringCheck.append(myList.get(i + j));
-            }
-            String s = stringCheck.toString();
-            if ( s.equals(firstString) || s.equals(secondString))
-                return true;
+    private boolean checkCanOperate(Stack<Character> stack, String toCheck, char currentCharacter){
+        if(stack.isEmpty()) {
+            stack.push(currentCharacter);
+            return false;
         }
-        return false;
+        StringBuilder builder = new StringBuilder();
+        builder.append(stack.peek());
+        builder.append(currentCharacter);
+        if(builder.toString().equals(toCheck)) {
+            stack.pop();
+            return true;
+        }
+        else{
+            stack.push(currentCharacter);
+            return false;
+        }
+
+
     }
+    private String stackToString(Stack<Character> stack){
+        StringBuilder builder = new StringBuilder();
+        for(char c: stack){
+            builder.append(c);
+        }
+        return builder.toString();
+    }
+
 
 
 }
